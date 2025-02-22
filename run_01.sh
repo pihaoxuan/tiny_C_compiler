5f9ef7a3222b1ab96d1ae534a9c3d79c133d5f0b
pi_haoxuan <haoxuanpi@163.com>
2025-02-22 21:39:25 +0800

    添加自动化运行脚本

---
+++ run_01.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#编译01.c
+gcc -g 01.c -o 01 
+
+#如果编译成功，则输出编译成功
+if [ $? -eq 0 ]; then
+    echo "01.c编译成功"
+else
+    echo "01.c编译失败"
+    exit 1
+fi
+
+#检测test.txt是否存在，存在则删除
+if [ -f "test.txt" ]; then
+    rm test.txt
+fi
+
+#运行01.c并将输出重定向到test.txt
+#./01 test.c >> test.txt
+./01 test.c
+
+#echo "01.c解析完成"
