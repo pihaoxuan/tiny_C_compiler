cb3fcf674df096314f97482a6b2196c2f6ef0589
pi_haoxuan <haoxuanpi@163.com>
2025-02-22 22:32:11 +0800

    修改bug：一旦读取到文件中的中文直接停机

--- 01.c
+++ 01.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <memory.h>
+#include <memory.h>
 #include <string.h>
-#include <fcntl.h>
-#define int long long
 
-// 全局数据结构
-int token;          // 当前解析的token值
-wchar_t *src;       // 源代码指针（使用宽字符处理多语言）
-wchar_t *old_src;   // 源代码起始地址备份
-int poolsize;       // 内存池大小（默认256KB）
-int line;           // 行号计数器
+int token;            // current token
+char *src, *old_src;  // pointer to source code string;
+int poolsize;         // default size of text/data/stack
+int line;             // line number
 
-// 词法分析核心函数
 void next() {
-    token = *src++; // 1. 读取当前字符 2. 指针前进
+    token = *src++;
+    return;
 }
 
-// pewpwd
-void expression(int level)
-{
+void expression(int level) {
     // do nothing
 }
-// 语法分析入口（当前仅实现字符循环读取）
+
 void program() {
-    next(); 
-    while (token > 0) { // 主解析循环（0表示字符串终止符）
-        printf("token is: %c\n", token); // 调试输出
+    next();                  // get next token
+    // while (token > 0)    //这样不能读取中文，因为中文的ASCII码大于0，转换成token的时候可能为负值
+    while (token != 0)      //改为token!=0
+    {
+        printf("token is: %c\n", token);
         next();
     }
 }
-int eval(){
-    //do nothing
+
+
+int eval() { // do nothing yet
     return 0;
 }
 
-// 主程序流程
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int i, fd;
 
     argc--;
@@ -45,26 +42,26 @@ int main(int argc, char **argv) {
     poolsize = 256 * 1024; // arbitrary size
     line = 1;
 
-    if ((fd = open(*argv, 0)) < 0)
-    {
+    if ((fd = open(*argv, 0)) < 0) {
         printf("could not open(%s)\n", *argv);
         return -1;
     }
 
-    if (!(src = old_src = malloc(poolsize)))  //使用calloc初始化内存，代替malloc，初始化内存为0
-    // if(!(src = old_src = calloc(poolsize, sizeof(wchar_t))))
+    old_src = malloc(poolsize); // 分配内存,如果成功返回指针，否则返回NULL
+    if (old_src){
+        src = old_src;  //将old_src的地址赋值给src，方便后面next()函数使用
+    }
+    else
     {
-        printf("could not malloc(%lld) for source area\n", poolsize);
+        printf("could not malloc(%d) for source area\n", poolsize);
         return -1;
     }
 
     // read the source file
-    if ((i = read(fd, src, poolsize - 1)) <= 0)
-    {
-        printf("read() returned %lld\n", i);
+    if ((i = read(fd, src, poolsize-1)) <= 0) {
+        printf("read() returned %d\n", i);
         return -1;
     }
-    printf("i=%d\n", i);
     src[i] = 0; // add EOF character
     close(fd);
 
