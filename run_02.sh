d15333468550e1a055adb54b2a99c95d44ee34c9
pi_haoxuan <haoxuanpi@163.com>
2025-02-23 17:04:23 +0800

    修改输出提示语

---
+++ run_02.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#编译01.c
+gcc -g 02.c -o 02 
+
+#如果编译成功，则输出编译成功
+if [ $? -eq 0 ]; then
+    echo "02.c编译成功"
+else
+    echo "02.c编译失败"
+    exit 1
+fi
+
+#检测test.txt是否存在，存在则删除
+if [ -f "02test.txt" ]; then
+    rm 02test.txt
+fi
+
+#运行02.c并将输出重定向到02test.txt
+./02 test.c >> 02test.txt
+
+
+echo "02.c解析test.c完成,生成文件02test.txt"
